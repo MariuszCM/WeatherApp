920e90fd1a39a09154ae19375697c27d7dbdcd73
MariuszCM <75585975+MariuszCM@users.noreply.github.com>
2023-05-08 22:32:17 +0200

    dodanie kolejnych di

---
+++ app/src/main/java/com/celinski/weatherapp/dependencyInjection/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.celinski.weatherapp.dependencyInjection
+
+import com.celinski.weatherapp.data.gps.DefaultLocationTracker
+import com.celinski.weatherapp.domain.gps.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
